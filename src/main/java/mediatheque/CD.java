62c06a98aaaa0ea9e9ee8240a6f126b27c81bf6f
nourelhoudabencheikh <lhoudanour584@gmail.com>
2023-10-10 22:58:08 +0200

    MediathequeMaven

---
+++ src/main/java/mediatheque/CD.java
@@ -0,0 +1,34 @@
+package mediatheque;
+
+public class CD extends Item {
+	private int numberOfTracks;
+
+	public CD(int numberOfTracks, String title) {
+		super(title);
+		this.numberOfTracks = numberOfTracks;
+	}
+
+	public int getNumberOfTracks() {
+		return numberOfTracks;
+	}
+
+	public void setNumberOfTracks(int numberOfTracks) {
+		this.numberOfTracks = numberOfTracks;
+	}
+
+	public void print() {
+		System.out.println(this);
+	}
+
+	@Override
+	public String toString() {
+		return "CD{" + super.toString() + ",numberOfTracks=" + numberOfTracks + '}';
+	}
+
+	@Override
+
+	public void accept(ItemVisitors v) {
+		v.visit(this);
+	}
+	
+}
---
+++ src/main/java/mediatheque/Mediatheque.java
@@ -0,0 +1,38 @@
+package mediatheque;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Mediatheque {
+	private final List<Item> items = new LinkedList<>();
+
+	public void addItem(Item i) {
+		items.add(i);
+	}
+	
+	public void printCatalog() {
+		CatalogVisitors myVisitor = new CatalogVisitors();
+		for (Item i : items)
+			i.accept(myVisitor);
+	}
+	
+	public void printOnlyBooks() {
+		BookVisitors myVisitor = new BookVisitors();
+		for (Item i : items)
+			i.accept(myVisitor);
+		/*
+		//avec instanceof
+		for (Item i : items)
+			if (i instanceof Book)
+				System.out.println(i);
+		*/
+	}
+
+	public void printOnlyCDs() {
+		CDVisitors myVisitor = new CDVisitors();
+		for (Item i : items)
+			i.accept(myVisitor);
+	}
+
+}
